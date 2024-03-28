4c654c1ed7d00b5ee65864ab38e68f7a02647bde
efratburshtain <efrat1574@gmail.com>
2024-03-28 21:24:53 +0200

    Perfect_integration_between_mailgan_and_nest.js

---
+++ src/modules/email.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { EmailService } from 'src/services/email.service';
+import { MessageService } from 'src/services/message.service';
+
+@Module({
+  providers: [EmailService,MessageService]
+})
+export class MailModule{}
--- src/modules/mail.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MailService } from 'src/services/mail.service';
-
-@Module({
-  providers: [MailService]
-})
-export class MailModule{}
