0889a187079570a3885bd671b8a6c24bd2cdee68
Rachely-Kremer <rachelkr2580@gmail.com>
2024-05-20 20:26:23 +0300

    Employee invitation email

--- src/services/rabbit-consumer/rabbit-consumer.service.ts
+++ src/services/rabbit-consumer/rabbit-consumer.service.ts
@@ -1,18 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { Message,Ctx,Bind,RmqContext } from 'amqplib';
-
+import { Message, Ctx, Bind, RmqContext } from 'amqplib';
+import { EmailService } from '../email.service';
 @Injectable()
 export class RabbitConsumerService {
+  constructor(private readonly emailService: EmailService) { }
+
   @MessagePattern('message_queue')
-  async handleEvent(@Payload() message: Message) {
+  async handleEvent(@Payload() message: Message, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+
     console.log('Received message:', message.content.toString());
     // טיפול בהודעה כאן
+
+    // Parse the message content to JSON
+    const messageData = JSON.parse(message.content.toString());
+    // Extract employee details from the message
+
+    const employeeName = messageData.employeeName;
+    const employeeEmail = messageData.employeeEmail;
+    const invitationLink = messageData.invitationLink;
+
+    // Send the email
+    await this.emailService.sendEmail(employeeEmail, 'Welcome to Our Company!', emailContent)
+
+    // Acknowledge the message
+    channel.ack(originalMsg);
   }
 }
-
-
-
 
 
 
